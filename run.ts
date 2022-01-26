e84685363306e560f436cc234ac04ee0b04ba504
Sreevishnu <sreeab28@gmail.com>
2022-01-26 16:19:56 +0530

    added run & flag logic

--- run.ts
+++ run.ts
@@ -1,7 +1,23 @@
-console.log("Args:", Deno.args);
+import { compareObjects } from './modules/compare.ts';
+import { readFromFile, writeToFile } from './modules/utils.ts';
+
+// console.log("Args:", Deno.args);
 
 const file1 = Deno.args[0];
 const file2 = Deno.args[1];
-const optionIdx = Deno.args.findIndex(arg => arg.toLowerCase() === "-o"); // since there is only 1 optional fucntionality at the moment
-const option = Deno.args[optionIdx];
-const outputFile = Deno.args[optionIdx + 1];
+const keyIdx = Deno.args.findIndex(arg => arg.toLowerCase() === "-k");
+if(keyIdx === -1)
+  throw new Error("Missing flag '-k': Key not specified");
+const key = Deno.args[keyIdx + 1];
+const outputIdx = Deno.args.findIndex(arg => arg.toLowerCase() === "-o");
+
+const fileContent1 = await readFromFile(file1);
+const fileContent2 = await readFromFile(file2);
+
+const extractedObjs = compareObjects(key, fileContent1, fileContent2)
+
+if(outputIdx !== -1){
+  const outputFile = Deno.args[outputIdx + 1];
+  console.log("Found output '-o' flag. Output file:", outputFile);
+  await writeToFile(outputFile, extractedObjs);
+}
