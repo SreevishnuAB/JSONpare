2f6c92296da10aa7bbe348e0f7bec884cc3877bb
Sreevishnu <sreeab28@gmail.com>
2022-01-26 17:08:52 +0530

    added logging for read file content, added filter by keys logic

--- modules/utils.ts
+++ modules/utils.ts
@@ -1,6 +1,7 @@
 export async function readFromFile(filePath: string): Promise<Record<string,unknown>[]>{
   try {
     const fileContent = await Deno.readTextFile(filePath);
+    console.log("Read file contents:", fileContent);
     return JSON.parse(fileContent);
   } catch (error) {
     console.error("Oops! Something went wrong:", error);
@@ -16,4 +17,16 @@ export async function writeToFile(filePath: string, fileContent: Record<string, 
     console.error("Oops! Something went wrong:", error);
     throw error;
   }
+}
+
+export function filterObjects(keys: string[], objects: Record<string, unknown>[]): Record<string, unknown>[]{
+  
+  const filterObjectWithKeys = (keys: string[], object: Record<string, unknown>)=>{
+    const newObj: Record<string, unknown> = {}
+    for(const key of keys)
+      newObj[key] = object[key];
+    return newObj;
+  }
+  const filteredObjs = objects.map(obj => filterObjectWithKeys(keys, obj));
+  return filteredObjs;
 }
